012ed0ca81ad0f06c09efebeeb803a2ff7fb770a
Akatsuki <dreandawn@gmail.com>
2018-03-10 11:53:40 +0800

    daily update

--- Archive/kuangbin/8/A.cpp
+++ Archive/kuangbin/8/A.cpp
@@ -13,9 +13,6 @@
 #include <map>
 #include <functional>
 #include <bitset>
-//#include <unordered_map>
-//#include <unordered_set>
-
 using namespace std;
 #define ll long long
 #define ull unsigned long long
@@ -24,106 +21,80 @@ using namespace std;
 #define mk make_pair
 #define fi first
 #define se second
-#define mid(l,r) ((l)+((r)-(l))/2)
 #define ALL(A) A.begin(), A.end()
-#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
-#define repr(i,n) for(int (i)=(int)(n);(i)>=0;(i)--)
-#define repab(i,a,b) for(int (i)=(int)(a);(i)<=(int)(b);(i)++)
-#define reprab(i,a,b) for(int (i)=(int)(a);(i)>=(int)(b);(i)--)
+#define sqr(x) ((x)*(x))
 #define sc(x) scanf("%d", &x)
-#define pr(x) printf("x:%d\n", x)
+#define pr(x) printf(">>>"#x":%d\n", x)
 #define fastio ios::sync_with_stdio(0),cin.tie(0)
 #define frein freopen("in.txt", "r", stdin)
 #define freout freopen("out.txt", "w", stdout)
-#define freout1 freopen("out1.txt", "w", stdout)
-#define lson(rt) (rt*2+1)
-#define rson(rt) (rt*2+2)
-#define lb puts("")
-#define debug cout<<"???"<<endl
-#define PI 3.1415926535897932
+#define debug cout<<">>>STOP"<<endl
 const ll mod = 1000000007;
-const ll INF = 0x3f3f3f3f;
-const ll INF64 = 1223372036854775807;
+const int INF = 0x3f3f3f3f;
+const ll INF64 = 0x3f3f3f3f3f3f3f3f;
 const double eps = 1e-7;
 template<class T> T gcd(T a, T b){if(!b)return a;return gcd(b,a%b);}
-int n, m;
 const int maxn = 110;
-const int maxm = 1e5 + 10;
+const int maxe = 20010;
 struct edge
 {
-    int u, v, w;
-}es[maxm];
-int del;
-vector<int> vec;
-int par[maxn];
-bool flag;
+    int v, w, nxt;
+}es[maxe];
+int path[maxn][maxn];
+bool used[maxe];
+int mincost[maxn];
+int head[maxn];
+bool vis[maxn];
+int pre[maxn];
+int id[maxn];
+int n, m, cnt;
 
-
-int find(int x)
+void init()
 {
-    if(par[x] == x)return x;
-    else return par[x] = find(par[x]);
+    cnt = 0;
+    memset(head, -1, sizeof(head));
 }
 
-void unite(int u, int v)
+void add_edge(int u, int v, int w)
 {
-    u = find(u);
-    v = find(v);
-    if(u == v)return;
-    par[u] = v;
+    es[cnt].v = v, es[cnt].w = w, es[cnt].nxt = head[u];
+    head[u] = cnt++;
+    es[cnt].v = u, es[cnt].w = w, es[cnt].nxt = head[v];
+    head[v] = cnt++;
 }
 
-bool cmp(edge a, edge b)
+int prim()
 {
-    return a.w < b.w;
-}
-
-int kru()
-{
-    int res = 0;
-    for(int i = 0; i <= n; i++)par[i] = i;
-    for(int i = 0; i < m; i++){
-        if(del == i)continue;
-        if(find(es[i].u) != find(es[i].v)){
-            unite(es[i].u, es[i].v);
-            //printf(" %d %d %d\n", es[i].u, es[i].v, es[i].w);
-            res += es[i].w;
-            if(flag)vec.pb(i);
+    memset(vis, 0, sizeof(vis));
+    memset(path, 0, sizeof(path));
+    memset(mincost, INF, sizeof(mincost));
+    memset(used, 0, sizeof(used));
+    int ret = 0;
+    mincost[1] = 0;
+    while(1){
+        int u = -1;
+        for(int i = 1; i <= n; i++){
+            if(!vis[i] && (u == -1 || mincost[i] < mincost[u]))u = i;
+        }
+        if(u == -1)break;
+        ret += mincost[u];
+        vis[u] = 1;
+        if(u != 1)used[id[u]] = used[id[u]^1] = 1;
+        for(int i = head[u]; ~i; i = es[i].nxt){
+            int v = es[i].v, w = es[i].w;
+            if(mincost[v] > w){
+                mincost[v] = w;
+                pre[v] = u;
+                id[v] = i;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(vis[i] && i != u){
+                path[u][i] = path[i][u] = max(path[i][pre[u]], mincost[u]);
+            }
         }
     }
-    for(int i = 1; i <= n; i++){
-        if(find(i) != find(1))return INF;
-    }
-    return res;
-}
-
-void work()
-{
-    flag = 1;
-    vec.clear();
-    sc(n);sc(m);
-    for(int i = 0; i < m; i++){
-        int u, v, w;
-        sc(u);sc(v);sc(w);
-        es[i].u = u, es[i].v = v, es[i].w = w;
-    }
-    sort(es, es + m, cmp);
-//    for(int i = 0; i < m; i++){
-//        printf("%d %d %d\n", es[i].u, es[i].v, es[i].w);
-//    }
-    del = -1;
-    int ans = kru();
-    flag = 0;
-    for(int i = 0; i < (int)vec.size(); i++){
-        del = vec[i];
-        int now = kru();
-        if(now == ans){
-            printf("Not Unique!\n");
-            return;
-        }
-    }
-    printf("%d\n", ans);
-    return;
+    return ret;
 }
 
 int main()
@@ -131,9 +102,29 @@ int main()
     int T;
     sc(T);
     while(T--){
-        work();
+        init();
+        sc(n); sc(m);
+        int u, v, w;
+        for(int i = 0; i < m; i++){
+            sc(u); sc(v); sc(w);
+            add_edge(u, v, w);
+        }
+        int mst = prim();
+        bool unq = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = head[i]; ~j; j = es[j].nxt){
+                if(!used[j]){
+                    if(es[j].w == path[i][es[j].v]){
+                        unq = 0;
+                        break;
+                    }
+                }
+            }
+            if(!unq)break;
+        }
+        if(unq)printf("%d\n", mst);
+        else printf("Not Unique!\n");
     }
-	return 0;
+    return 0;
 }
-
 
--- Archive/kuangbin/8/B.cpp
+++ Archive/kuangbin/8/B.cpp
@@ -1,21 +1,4 @@
-#include <iostream>
-#include <cstdio>
-#include <cctype>
-#include <algorithm>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <stack>
-#include <sstream>
-#include <queue>
-#include <map>
-#include <functional>
-#include <bitset>
-//#include <unordered_map>
-//#include <unordered_set>
-
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define ull unsigned long long
@@ -24,77 +7,76 @@ using namespace std;
 #define mk make_pair
 #define fi first
 #define se second
-#define mid(l,r) ((l)+((r)-(l))/2)
 #define ALL(A) A.begin(), A.end()
-#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
-#define repr(i,n) for(int (i)=(int)(n);(i)>=0;(i)--)
-#define repab(i,a,b) for(int (i)=(int)(a);(i)<=(int)(b);(i)++)
-#define reprab(i,a,b) for(int (i)=(int)(a);(i)>=(int)(b);(i)--)
+#define sqr(x) ((x)*(x))
 #define sc(x) scanf("%d", &x)
-#define pr(x) printf("x:%d\n", x)
+#define pr(x) printf(">>>"#x":%d\n", x)
 #define fastio ios::sync_with_stdio(0),cin.tie(0)
 #define frein freopen("in.txt", "r", stdin)
 #define freout freopen("out.txt", "w", stdout)
-#define freout1 freopen("out1.txt", "w", stdout)
-#define lson(rt) (rt*2+1)
-#define rson(rt) (rt*2+2)
-#define lb puts("")
-#define debug cout<<"???"<<endl
-#define PI 3.1415926535897932
+#define debug cout<<">>>STOP"<<endl
 const ll mod = 1000000007;
-const ll INF = 2123456789;
-const ll INF64 = 1223372036854775807;
+const int INF = 0x3f3f3f3f;
+const ll INF64 = 0x3f3f3f3f3f3f3f3f;
 const double eps = 1e-7;
 template<class T> T gcd(T a, T b){if(!b)return a;return gcd(b,a%b);}
-const int maxn = 1000 + 10;
-int n;
-int x[maxn];
-int y[maxn];
-int p[maxn];
-double cost[maxn][maxn];
+const int maxn = 1010;
+const int maxe = 2000100;
+double mp[maxn][maxn];
+bool used[maxn][maxn];
 double path[maxn][maxn];
 double mincost[maxn];
-bool used[maxn][maxn];
 bool vis[maxn];
+double x[maxn], y[maxn];
+int p[maxn];
 int pre[maxn];
+int n;
 
-double dist(int id1, int id2)
+void init()
 {
-    double res = (x[id1]-x[id2])*(x[id1]-x[id2]) + (y[id1]-y[id2])*(y[id1]-y[id2]);
-    return sqrt(res);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++)mp[i][j] = INF;
+    }
+}
+
+double dis(int i, int j)
+{
+    return sqrt(sqr(x[i]-x[j])+sqr(y[i]-y[j]));
 }
 
 double prim()
 {
-    double res = 0;
-    fill(mincost, mincost + n + 1, 1.0*INF);
-    memset(used, 0, sizeof(used));
     memset(vis, 0, sizeof(vis));
-    memset(path, 0, sizeof(path));
-    memset(pre, 0, sizeof(pre));
-    mincost[1] = 0;
+    memset(used, 0, sizeof(used));
+    for(int i = 0; i < n; i++){
+        mincost[i] = INF;
+        for(int j = 0; j < n; j++){
+            path[i][j] = 0;
+        }
+    }
+    mincost[0] = 0;
+    double ret = 0;
     while(1){
         int u = -1;
-        for(int i = 1; i <= n; i++){
-            if(!vis[i] && (u == -1 || mincost[i] < mincost[u])){
-                u = i;
-            }
+        for(int i = 0; i < n; i++){
+            if(!vis[i] && (u==-1 || mincost[i] < mincost[u]))u = i;
         }
         if(u == -1)break;
         vis[u] = 1;
-        used[u][pre[u]] = used[pre[u]][u] = 1;
-        res += mincost[u];
-        for(int i = 1; i <= n; i++){
-            if(vis[i] && i != u){
-                path[u][i] = path[i][u] = max(path[i][pre[u]], mincost[u]);
-            }
-            if(!vis[i] && mincost[i] > cost[u][i]){
-                mincost[i] = cost[u][i];
+        ret += mincost[u];
+        if(u != 0)used[u][pre[u]] = used[pre[u]][u] = 1;
+        for(int i = 0; i < n; i++){
+            if(!vis[i] && mincost[i] > mp[u][i]){
+                mincost[i] = mp[u][i];
                 pre[i] = u;
             }
+            if(i != u && vis[i]){
+                path[i][u] = path[u][i] = max(path[i][pre[u]], mincost[u]);
+            }
         }
+
     }
-    return res;
+    return ret;
 }
 
 int main()
@@ -102,30 +84,26 @@ int main()
     int T;
     sc(T);
     while(T--){
+        init();
         sc(n);
-        for(int i = 1; i <= n; i++){
-            sc(x[i]);sc(y[i]);sc(p[i]);
+        for(int i = 0; i < n; i++){
+            scanf("%lf%lf%d", &x[i], &y[i], &p[i]);
         }
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                cost[i][j] = dist(i, j);
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                mp[i][j] = mp[j][i] = dis(i, j);
             }
         }
-        double mst = prim();
         double ans = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = i + 1; j <= n; j++){
-                if(used[i][j]){
-                    ans = max(ans, 1.0*(p[i]+p[j])/(mst - cost[i][j]));
-                }
-                else{
-                    ans = max(ans, 1.0*(p[i]+p[j])/(mst - path[i][j]));
-                }
+        double mst = prim();
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(used[i][j])ans  = max(ans, (p[i]+p[j])/(mst-mp[i][j]));
+                else ans = max(ans, (p[i]+p[j])/(mst-path[i][j]));
             }
         }
-         printf("%.2f\n", ans);
+        printf("%.2f\n", ans);
     }
-	return 0;
+    return 0;
 }
-
 
