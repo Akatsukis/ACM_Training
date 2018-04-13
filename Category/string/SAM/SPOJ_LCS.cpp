6aa33d1cc126c2ae09f59944e3958067c50eded6
Akatsuki <dreandawn@gmail.com>
2018-04-13 14:17:41 +0800

    update SAM

--- Category/string/SAM/SPOJ_LCS.cpp
+++ Category/string/SAM/SPOJ_LCS.cpp
@@ -26,7 +26,7 @@ char s[maxn], t[maxn];
 struct Node
 {
     int nxt[maxn][maxlen], par[maxn], len[maxn];
-    int idx, now;
+    int idx, lst;
     void init(int u)
     {
         memset(nxt[u], 0, sizeof(nxt[u]));
@@ -34,20 +34,57 @@ struct Node
     }
     void init()
     {
-        idx = now = 0;
-        now = ++idx;
-        init(now);
+        idx = lst = 0;
+        lst = ++idx;
+        init(lst);
     }
-    void extend(int x)
+    void extend(int c)
     {
-        int rt = now, np = ++idx;
+        int rt = lst, np = ++idx;
         init(np);
         len[np] = len[rt]+1;
-        while(rt && !nxt[rt][x])nxt[rt][x] = np, rt = par[rt];
-        if(!rt)par[rt] = 1;
+        for(; rt&&!nxt[rt][c]; rt = par[rt])nxt[rt][c] = np;
+        if(!rt)par[np] = 1;
         else{
-            
+            int q = nxt[rt][c];
+            if(len[q] == len[rt]+1)par[np] = q;
+            else{
+                int nq = ++idx;
+                par[nq] = par[q];
+                len[nq] = len[rt]+1;
+                memcpy(nxt[nq], nxt[q], sizeof(nxt[q]));
+                par[np] = par[q] = nq;
+                for(; rt&&nxt[rt][c]==q; rt = par[rt])nxt[rt][c] = nq;
+            }
         }
+        lst = np;
+    }
+    void solve()
+    {
+        int rt = 1, n = strlen(t);
+        int ans = 0, now = 0;
+        for(int i = 0; i < n; i++){
+            int c = t[i]-'a';
+            if(nxt[rt][c]) {
+                rt = nxt[rt][c];
+                now++;
+                //printf("rt=%d, now=%d c=%d\n", rt, now, c);
+                ans = max(ans, now);
+            }
+            else{
+                while(rt && !nxt[rt][c])rt = par[rt];
+                if(nxt[rt][c]){
+                    now = len[rt]+1;
+                    rt = nxt[rt][c];
+                    ans = max(ans, now);
+                }
+                else{
+                    rt = 1;
+                    now = 0;
+                }
+            }
+        }
+        printf("%d\n", ans);
     }
 }SAM;
 
@@ -59,6 +96,7 @@ int main()
     for(int i = 0; i < n; i++){
         SAM.extend(s[i]-'a');
     }
+    SAM.solve();
     return 0;
 }
 
