94fb9527bad10d0b967f867ffcba79d8c62676a7
Akatsuki <dreandawn@gmail.com>
2018-04-30 11:48:23 +0800

    update 2015 Shenyang Online

--- Online/2015/Shenyang/B.cpp
+++ Online/2015/Shenyang/B.cpp
@@ -13,32 +13,54 @@ using namespace std;
 #define pr(x) printf(">>>"#x":%d\n", x)
 #define fastio ios::sync_with_stdio(0),cin.tie(0)
 #define debug cout<<">>>STOP"<<endl
-const int INF = 0x3f3f3f3f;
-const ll INF64 = 0x3f3f3f3f3f3f3f3f;
-const double eps = 1e-7;
 template<class T> T gcd(T a, T b){if(!b)return a;return gcd(b,a%b);}
-const int maxn = 2e6+10;
-int f[maxn];
-ll n;
+const int sz = 2;
 int mod;
-
-int solve()
+struct Mat
 {
-    f[0] = 10%mod;
-    f[1] = 98%mod;
-    for(int i = 2; i < maxn; i++){
-        f[i] = (f[i-1]*10-f[i-2]+mod)%mod;
-        if(f[i] == f[1] && f[i-1] == f[0])return i-1;
+    ll a[sz][sz];
+    Mat()
+    {
+        memset(a, 0, sizeof(a));
     }
-    return 0;
+    void set()
+    {
+        memset(a, 0, sizeof(a));
+        for(int i = 0; i < sz; i++)a[i][i] = 1;
+    }
+    Mat operator * (const Mat &b)
+    {
+        Mat ret;
+        for(int i = 0; i < sz; i++){
+            for(int k = 0; k < sz; k++){
+                for(int j = 0; j < sz; j++){
+                    ret.a[i][j] = (ret.a[i][j]+a[i][k]*b.a[k][j])%mod;
+                    ret.a[i][j] = (ret.a[i][j]+mod)%mod;
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+int qpow(ll a, int n, int y)
+{
+    ll ret = 1;
+    while(n){
+        if(n&1)ret = ret*a%y;
+        a = a*a%y;
+        n >>= 1;
+    }
+    return ret;
 }
 
-int qpow(int a, int n, int x)
+Mat mpow(Mat a, int n)
 {
-    int ret = 1;
+    Mat ret;
+    ret.set();
     while(n){
-        if(n&1)ret = ret*a%x;
-        a = a*a%x;
+        if(n&1)ret = ret*a;
+        a = a*a;
         n >>= 1;
     }
     return ret;
@@ -49,11 +71,15 @@ int main()
     int T, kase = 1;
     sc(T);
     while(T--){
-        scanf("%lld%d", &n, &mod);
-        int x = solve();
-        int ans = (f[qpow(2, n, x)]-1+mod)%mod;
+        int x;
+        scanf("%d%d", &x, &mod);
+        int n = qpow(2, x, (mod+1)*(mod-1))+1;
+        Mat ret;
+        ret.a[0][0] = 10, ret.a[0][1] = -1, ret.a[1][0] = 1;
+        ret = mpow(ret, n-2);
+        int ans = (ret.a[0][0]*98+ret.a[0][1]*10-1)%mod;
+        ans = (ans+mod)%mod;
         printf("Case #%d: %d\n", kase++, ans);
-
     }
     return 0;
 }
